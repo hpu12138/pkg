30fcb7147699a69faed707c50f55ff88ec8ed4a9
hpu12138 <phmhappier@163.com>
2019-10-31 19:04:04 -0400

    Add files via upload

---
+++ scsim/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 19 15:30:59 2019
+
+@author: Lenovo
+"""
+
+
+from .dataset import CsvDataset, GeneExpressionDataset, DownloadableDataset
+from .posterior import Posterior
+from .trainer import Trainer
+from .modules import Encoder, DecoderSCVI
+from .vae import VAE
+from .cortex import CortexDataset
